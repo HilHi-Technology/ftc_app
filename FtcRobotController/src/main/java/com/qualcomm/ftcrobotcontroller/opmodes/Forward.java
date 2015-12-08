f81200eb5d3eca085d7c0d65cb62648fabc88206
Jonathan Alexander <notdisliked@gmail.com>
2015-12-07 16:31:06 -0800

    Add improved autonomous program

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Forward.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Forward.java
@@ -1,26 +1,109 @@
 package com.qualcomm.ftcrobotcontroller.opmodes;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+
+import java.util.concurrent.ExecutionException;
 
 
 //Created by Jonathan on 9/28/2015
 
-public class Forward extends OpMode {
+public class Forward extends LinearOpMode {
 
-    DcMotor forward;
+    DcMotor left;
+    DcMotor right;
 
     public Forward() {
 
     }
 
     @Override
-    public void init() {
-        forward = hardwareMap.dcMotor.get("forward");
+    public void runOpMode(){
+
+        mapHardwareLeft();
+        mapHardwareRight();
+
+        newWaitForStart();
+
+        right.setPower(1);
+
+        try {
+            sleep(1000);
+            left.setPower(1);
+        } catch (Exception e) {
+
+        }
+
+        left.setPower(0);
+        right.setPower(0);
     }
 
-    @Override
-    public void loop() {
-        forward.setPower(1);
+    private void goForward(int milliseconds) {
+        left.setPower(1);
+        right.setPower(1);
+
+        try {
+            sleep(milliseconds);
+        } catch (Exception e) {
+
+        }
+        left.setPower(0);
+        right.setPower(0);
+    }
+
+    private void goBackward(int milliseconds) {
+        left.setPower(-1);
+        right.setPower(-1);
+
+        try {
+            sleep(milliseconds);
+        } catch (Exception e) {
+
+        }
+        left.setPower(0);
+        right.setPower(0);
+    }
+
+    private void turnRight(int milliseconds) {
+        left.setPower(1);
+        right.setPower(-1);
+
+        try {
+            sleep(milliseconds);
+        } catch (Exception e) {
+
+        }
+        left.setPower(0);
+        right.setPower(0);
+    }
+
+    private void turnLeft(int milliseconds) {
+        left.setPower(-1);
+        right.setPower(1);
+
+        try {
+            sleep(milliseconds);
+        } catch (Exception e) {
+
+        }
+        left.setPower(0);
+        right.setPower(0);
+    }
+
+    private void newWaitForStart() {
+        try {
+            waitForStart();
+        } catch (Exception e) {
+
+        }
+    }
+
+    private void mapHardwareLeft() {
+        left = hardwareMap.dcMotor.get("left");
+    }
+
+    private void mapHardwareRight() {
+        right = hardwareMap.dcMotor.get("right");
     }
 }
