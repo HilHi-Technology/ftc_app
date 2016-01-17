ff3bdf0c9582ddddc09573516c28cc3d76f96d15
MDA <mda31415@gmail.com>
2016-01-16 23:10:19 -0800

    Combined old autonomous with new encoder autonomous

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Forward.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Forward.java
@@ -1,91 +1,229 @@
 package com.qualcomm.ftcrobotcontroller.opmodes;
 
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
 import com.qualcomm.robotcore.hardware.DcMotor;
+
+import com.qualcomm.robotcore.hardware.DcMotorController;
+
+import com.qualcomm.robotcore.util.Range;
+
 
 import java.util.concurrent.ExecutionException;
 
 
+
 //Created by Jonathan on 9/28/2015
+
 
 public class Forward extends LinearOpMode {
 
-    DcMotor left;
-    DcMotor right;
+    
 
-    public Forward() {
+DcMotor left;
+    
+DcMotor right;
+
+ 
+
+Servo climberDump   
+
+
+private int turn90 = 1230;
+
+   
+
+public Forward() {
 
     }
 
-    @Override
-    public void runOpMode(){
+    
 
-        mapHardwareLeft();
-        mapHardwareRight();
+@Override
+    
+public void runOpMode() {
 
-        newWaitForStart();
+        
 
-        goForward(1000);
+mapHardware();
 
+        
 
-    }
+left.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+        
+right.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
 
-    private void goForward(int milliseconds) {
-        left.setPower(1);
-        right.setPower(1);
+        
 
+newWaitForStart();
+
+      
+
+climberDump.setPosition(1);
+
+goForward(5355, 0.4);
+
+        
+
+pause(2000);
+
+        
+
+turnRight(1095, 0.3);
+
+        
+
+pause(2000);
+
+        
+
+goForward(3300, 0.3);
+
+        
+
+pause(2000);
+
+        
+
+goForward(450, 0.1);
+
+climberDump.setPosition(0);
+    
+}
+
+    
+
+private void pause(int milliseconds) {
         try {
             sleep(milliseconds);
         } catch (Exception e) {
 
         }
-        left.setPower(0);
-        right.setPower(0);
     }
 
-    private void goBackward(int milliseconds) {
-        left.setPower(-1);
-        right.setPower(-1);
+    private void goForward (int distance, double power) {
+        right.setTargetPosition(distance);
+        left.setTargetPosition(distance);
 
-        try {
-            sleep(milliseconds);
-        } catch (Exception e) {
+        left.setMode(DcMotorController.RunMode.RUN_TO_POSITION);
+        right.setMode(DcMotorController.RunMode.RUN_TO_POSITION);
+
+        left.setPower(power);
+        right.setPower(power);
+
+
+
+        while (Math.abs(right.getCurrentPosition()) < distance) {
 
         }
+
+        telemetry.addData("2", String.format("%d", right.getTargetPosition()));
+
         left.setPower(0);
         right.setPower(0);
+
+        left.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+        right.setMode(DcMotorController.RunMode.RESET_ENCODERS);
     }
 
-    private void turnRight(int milliseconds) {
-        left.setPower(1);
-        right.setPower(-1);
+    private void goBackward(int distance, double power) {
+        right.setTargetPosition(-distance);
+        left.setTargetPosition(-distance);
 
-        try {
-            sleep(milliseconds);
-        } catch (Exception e) {
+        left.setMode(DcMotorController.RunMode.RUN_TO_POSITION);
+        right.setMode(DcMotorController.RunMode.RUN_TO_POSITION);
+
+        left.setPower(power);
+        right.setPower(power);
+
+
+        while (Math.abs(right.getCurrentPosition()) < distance) {
 
         }
+
+        telemetry.addData("2", String.format("%d", right.getTargetPosition()));
+
         left.setPower(0);
         right.setPower(0);
+
+        left.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+        right.setMode(DcMotorController.RunMode.RESET_ENCODERS);
     }
 
-    private void turnLeft(int milliseconds) {
-        left.setPower(-1);
-        right.setPower(1);
+    private void turnRight(int distance, double power) {
+        right.setTargetPosition(-distance);
+        left.setTargetPosition(distance);
 
-        try {
-            sleep(milliseconds);
-        } catch (Exception e) {
+        left.setMode(DcMotorController.RunMode.RUN_TO_POSITION);
+        right.setMode(DcMotorController.RunMode.RUN_TO_POSITION);
+
+        left.setPower(power);
+        right.setPower(power);
+
+
+
+        while (Math.abs(right.getCurrentPosition()) < distance) {
 
         }
+
+        telemetry.addData("2", String.format("%d", right.getTargetPosition()));
+
         left.setPower(0);
         right.setPower(0);
+
+        left.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+        right.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+    }
+
+    private void turnLeft(int distance, double power) {
+        right.setTargetPosition(distance);
+        left.setTargetPosition(-distance);
+
+        left.setMode(DcMotorController.RunMode.RUN_TO_POSITION);
+        right.setMode(DcMotorController.RunMode.RUN_TO_POSITION);
+
+        left.setPower(power);
+        right.setPower(power);
+
+
+
+        while (Math.abs(right.getCurrentPosition()) < distance) {
+
+        }
+
+        telemetry.addData("2", String.format("%d", right.getTargetPosition()));
+
+        left.setPower(0);
+        right.setPower(0);
+
+        left.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+        right.setMode(DcMotorController.RunMode.RESET_ENCODERS);
     }
 
     private void newWaitForStart() {
         try {
             waitForStart();
+        } catch (Exception e) {
+
+        }
+    }
+
+    private void mapHardware() {
+        
+left = hardwareMap.dcMotor.get("left");
+        
+right = hardwareMap.dcMotor.get("right");
+
+        
+right.setDirection(DcMotor.Direction.REVERSE);
+climberDump = hardwareMap.servo.get("climberDump");
+    
+}
+}
+
         } catch (Exception e) {
 
         }
