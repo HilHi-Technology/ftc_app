d476ec57dec1f9a0294628da923972034a5be782
Jonathan Alexander <notdisliked@gmail.com>
2016-11-18 19:34:03 -0800

    ?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptVuforiaNavigation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptVuforiaNavigation.java
@@ -212,7 +212,7 @@ public class ConceptVuforiaNavigation extends LinearOpMode {
             leftMotor.setPower(0);
             rightMotor.setPower(0);
 
-            leftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+             leftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
             rightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
             sleep((int) sleepTime);
